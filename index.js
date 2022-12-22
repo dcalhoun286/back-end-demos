d49f2a53ec0d3a0c8850291d375f7993053d8697
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-22 09:42:37 -0800

    add solution to week 1 back end demo

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const { Dog } = require('./db');
 const { sequelize } = require('./db/db');
 const { Op } = require('sequelize');
 
+// RESTful is our architectrual style for organizing routes, and connecting routes to methods
+
+/**good examples:
+ * [VERB /noun -> description]
+ * GET /users -> shows all users
+ * GET /users/4 show single user (id=4 in db)
+ * POST /users -> create new user in db
+ * PUT /users/4 -> update the user with id=4 in db
+ * DELETE /users/4 -> delete user 4 from db
+ *    note: always user plural for consistency
+ */
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -36,7 +47,30 @@ app.delete('/dogs/:id', async (req, res, next) => {
 app.get('/dogs', async (req, res, next) => {
   try {
     //TO DO
-    res.send("need to complete this GET route");
+    // create our where object to build in the search queries
+    const where = {};
+    // loop through all of the possible queries (in array form)
+    const queriesArray = ['name', 'description', 'breed', 'color'];
+
+    for (const key of queriesArray) {
+      // check if a specific query string param is inside the req.body
+      if (req.query[key]) {
+        // if it does exist, then add to the where obj and create options
+        where[key] = {
+          // using symbol operators to search for any string containing what is inside that specific parameter
+          [Op.like]: `%${req.query[key]}%`
+        }
+      }
+    }
+
+    // const { name, breed, color, description } = req.query;
+    // const dogs = await Dog.findAll();
+
+    const dogs = await Dog.findAll({
+      where
+    });
+
+    res.send(dogs);
   } catch (error) {
     next(error)
   }
